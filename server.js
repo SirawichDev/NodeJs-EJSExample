f236c5f6285b991f82d0953c793f9c07b80f4863
exlovemom <sirawit0676@gmail.com>
2017-04-05 22:07:15 +0700

    comment

--- server.js
+++ server.js
@@ -16,23 +16,25 @@ res.render('pages/showCookie',{
 	cookie:a,
 });
 });
-//call cookie 
+//call cookie
 app.post('/ck_set', function(req, res) {
 res.cookie('a', 10)
 res.cookie('foo','bar')
 res.send('ok')
 })
+
 //session by counter
 app.use(function(req, res, next) {
    var sess = req.session
+
    if (sess.views) {
+
         sess.views++
 
-   } else {
+   } else{
        sess.views = 1
-
-   }
-   next()
+     }
+ next()
 })
 //call session
 app.post('/co',function(req,res){
@@ -41,6 +43,7 @@ res.render('pages/showSession',{
 	count:count,
 });
 });
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -49,6 +52,8 @@ app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res){
 	var a = res.cookie('a', 10)
+
+  req.session.views=0;
 	res.cookie('foo','bar')
 res.render('pages/home',{
 	Status:a,
@@ -56,13 +61,19 @@ res.render('pages/home',{
 });
 app.post('/', function(req, res){
   var num1 = parseInt(req.body.num1)+parseInt(req.body.num2);
+
 	res.render('pages/index', {
     num1: num1,
 
   });
 });
 
+app.get('/endses',function(req,res){
+  req.session.destroy();
+  res.send('OK');
+     // cannot access session here
 
+});
 
 
 app.listen(8080);
