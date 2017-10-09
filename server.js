9dd82d9bcd173af680b3ad7fbd22a68a43f961d6
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2017-10-09 08:26:50 +0700

    change server.js to es6 style

--- server.js
+++ server.js
@@ -1,80 +1,84 @@
 // load the things we need
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var cookieParser=require('cookie-parser')
-var session=require('express-session')
-app.use(bodyParser());
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 },
-                 resave:false,
-                 resaveUninitialized:false})
-	)
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+
+app.use(bodyParser())
+app.use(session({
+  secret: 'keyboard cat',
+  cookie: { 
+    maxAge: 60000
+  },
+  resave:false,
+  resaveUninitialized:false
+}))
+
 app.use(cookieParser('keyboard cat'))
-app.post('/ck_get', function(req, res) {
-var a = req.cookies.a;
-res.render('pages/showCookie',{
-	cookie:a,
-});
-});
+app.post('/ck_get', (req, res) => {
+  let a = req.cookies.a
+  res.render('pages/showCookie', {
+  	cookie:a,
+  })
+})
+
 //call cookie
-app.post('/ck_set', function(req, res) {
-res.cookie('a', 10)
-res.cookie('foo','bar')
-res.send('ok')
+app.post('/ck_set', (req, res) => {
+  res.cookie('a', 10)
+  res.cookie('foo','bar')
+  res.send('ok')
 })
 
 //session by counter
-app.use(function(req, res, next) {
-   var sess = req.session
+app.use((req, res, next) => {
+  let sess = req.session
 
-   if (sess.views) {
-
-        sess.views++
-
-   } else{
-       sess.views = 1
-     }
- next()
+  if (sess.views)
+    sess.views++
+  else
+    sess.views = 1
+  next()
 })
+
 //call session
-app.post('/co',function(req,res){
-	var count = req.session.views;
-res.render('pages/showSession',{
-	count:count,
-});
-});
+app.post('/co', (req,res) => {
+	let count = req.session.views
+  res.render('pages/showSession', {
+  	count:count,
+  })
+})
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // set init path at views
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views')
 
-app.get('/', function(req, res){
-	var a = res.cookie('a', 10)
+app.get('/', (req, res) => {
+	let a = res.cookie('a', 10)
 
-  req.session.views=0;
+  req.session.views = 0;
 	res.cookie('foo','bar')
-res.render('pages/home',{
-	Status:a,
-});
-});
-app.post('/', function(req, res){
-  var num1 = parseInt(req.body.num1)+parseInt(req.body.num2);
+  res.render('pages/home', {
+  	Status: a,
+  })
+})
+
+app.post('/', (req, res) => {
+  let num1 = parseInt(req.body.num1) + parseInt(req.body.num2);
 
 	res.render('pages/index', {
     num1: num1,
+  })
+})
 
-  });
-});
-
-app.get('/endses',function(req,res){
-  req.session.destroy();
-  res.send('OK');
-     // cannot access session here
-
-});
+app.get('/endses', (req,res) => {
+  req.session.destroy()
+  res.send('OK')
+  // cannot access session here
+})
 
 
-app.listen(8080);
-console.log('8080 is the magic port');
+app.listen(8080)
+console.log('8080 is the magic port')
